7152a94d5534ab7392d7caa9a426dcf37684c82a
Batya Sofer <batya0650@gmail.com>
2025-05-28 20:28:49 +0300

    Initial commit

---
+++ DocReferance.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace service;
+
+public partial class DocReferance
+{
+    public int Id { get; set; }
+
+    public int? DocId { get; set; }
+
+    public string? RefType { get; set; }
+
+    public string? Ref { get; set; }
+
+    public virtual Document? Doc { get; set; }
+}
---
+++ Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace service
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnState = new System.Windows.Forms.Button();
+            this.lblSate = new System.Windows.Forms.Label();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // btnState
+            // 
+            this.btnState.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnState.Location = new System.Drawing.Point(482, 299);
+            this.btnState.Name = "btnState";
+            this.btnState.Size = new System.Drawing.Size(165, 60);
+            this.btnState.TabIndex = 0;
+            this.btnState.UseVisualStyleBackColor = true;
+            this.btnState.Click += new System.EventHandler(this.start_Click);
+            // 
+            // lblSate
+            // 
+            this.lblSate.AutoSize = true;
+            this.lblSate.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblSate.Location = new System.Drawing.Point(500, 118);
+            this.lblSate.Name = "lblSate";
+            this.lblSate.Size = new System.Drawing.Size(0, 38);
+            this.lblSate.TabIndex = 3;
+            // 
+            // timer
+            // 
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1144, 575);
+            this.Controls.Add(this.lblSate);
+            this.Controls.Add(this.btnState);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button btnState;
+        private Label lblSate;
+        private System.Windows.Forms.Timer timer;
+    }
+}
